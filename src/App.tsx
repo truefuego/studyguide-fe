ffb0b90e522d9da74d0f3f4e9dd2c78d6bbe2bdf
truefuego <ankitsharma15092003@gmail.com>
2025-03-08 23:05:18 +0530

    Add - fonts and routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
+import HomePage from './pages/HomePage'
 
 function App() {
   return (
-    <>
-      <p>Hi</p>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<HomePage />}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
